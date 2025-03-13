d14f75c52fd1f64582a2215d66cb1c6463149468
skytechrick <ricksarkar2005@gmail.com>
2025-03-13 15:34:09 +0530

    Initiated React + Tailwindcss

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-blue-500 text-white flex items-center justify-center">
+      <h1 className="text-4xl font-bold">Hello Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
