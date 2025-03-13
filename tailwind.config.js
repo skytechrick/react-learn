2e8936e21af314b5da7a7677d6b3421b7046a753
skytechrick <ricksarkar2005@gmail.com>
2025-03-13 17:17:36 +0530

    TEmplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
